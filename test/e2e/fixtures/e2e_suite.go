17578710a61363c934effaa172899e1f9d59e037
Rohan Kumar <rohankmr414@gmail.com>
2022-07-26 10:47:37 +0530

    fix: patch BeforeTest DeleteResource logic
    Signed-off-by: Rohan Kumar <rohan@pipekit.io>

--- test/e2e/fixtures/e2e_suite.go
+++ test/e2e/fixtures/e2e_suite.go
@@ -3,10 +3,13 @@ package fixtures
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 	"time"
+
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/TwiN/go-color"
 	"github.com/stretchr/testify/suite"
@@ -132,6 +135,22 @@ func (s *E2ESuite) DeleteResources() {
 		{Version: "v1", Resource: "resourcequotas"},
 		{Version: "v1", Resource: "configmaps"},
 	}
+
+	pods := s.KubeClient.CoreV1().Pods(Namespace)
+	podList, err := pods.List(ctx, metav1.ListOptions{LabelSelector: common.LabelKeyCompleted + "=false"})
+	s.CheckError(err)
+
+	for _, pod := range podList.Items {
+		patch, err := json.Marshal(map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"finalizers": []string{},
+			},
+		})
+		s.CheckError(err)
+		_, err = pods.Patch(ctx, pod.Name, types.MergePatchType, patch, metav1.PatchOptions{})
+		s.CheckError(err)
+	}
+
 	for _, r := range resources {
 		for {
 			s.CheckError(s.dynamicFor(r).DeleteCollection(ctx, metav1.DeleteOptions{GracePeriodSeconds: pointer.Int64Ptr(2)}, metav1.ListOptions{LabelSelector: l(r)}))
